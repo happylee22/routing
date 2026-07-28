8a37969a1b0bd6cde00eeb8a898d25b1507a708b
pinky <happinesomueti@yahoo.com>
2026-07-28 14:08:32 +0100

    updated the login form

--- src/components/card/input.jsx
+++ src/components/card/input.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Input({ label, type, placeholder, value, onChange }) {
   return (
     <div className="flex flex-col ">
-      <label className="font:bold">{`${label}:`}</label>
+      <label className="font-bold">{`${label}:`}</label>
       <input
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="border border-gray-400 w-2/5"
+        className="border border-gray-400 w-full"
       />
     </div>
   );
--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -1,25 +1,55 @@
 import React from "react";
 import Input from "../card/input";
+import { useState } from "react";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (name && email && password) {
+      alert(`my name is ${name} my email ${email}`);
+      setName("");
+      setEmail("");
+      setPassword("");
+    } else {
+      alert("fill the form");
+    }
+  };
+
   return (
-    <div>
-      <form>
+    <div className="bg-gray-50 w-full h-screen">
+      <form
+        className="mr-auto ml-auto  mt-4 shadow w-[40%] flex flex-col p-4"
+        onSubmit={submitForm}
+      >
         <Input
           label={"Full Name"}
           type={"text"}
           placeholder={"enter Your Name"}
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         />
         <Input
           label={"Email"}
           type={"email"}
           placeholder={"e.g omuetihappy@gmail.com"}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
         <Input
           label={"Password"}
           type={"password"}
           placeholder={"Enter a valid password"}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
+        <button className="bg-blue-500 text-white" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
